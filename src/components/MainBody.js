4d5612487215ae1604f616f86e98cfc51a829d2b
GabeKrahulik <gabekrahulik@gmail.com>
2021-02-18 15:19:24 -0800

    h

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -40,7 +40,7 @@ const MainBody = ({ haikus }) => {
               <div>
                 <div style={{ width: '45vw' }}>
                   <h2 className="fade-p2">Mike Racecar</h2>
-                  <p className="fade-p2 bio-p uk-dropcap uk-text-left">His momma was a V6 and his daddy is the track.  Metaphorically speaking.  But he was born in the backseat of a stock car, that much we know for certain, because that car is now in a museum - the Mike Racecar Museum.  In 2007, he fell asleep during a race and still won.  He can't read nothing but the road, and the only number he can count to is "1st."</p>
+                  <p className="fade-p2 bio-p uk-text-left">His momma was a V6 and his daddy is the track.  Metaphorically speaking.  But he was born in the backseat of a stock car, that much we know for certain, because that car is now in a museum - the Mike Racecar Museum.  In 2007, he fell asleep during a race and still won.  He can't read nothing but the road, and the only number he can count to is "1st."</p>
                 </div>
                 <div>
                   <blockquote cite="#" className="fade-p2">
@@ -61,7 +61,7 @@ const MainBody = ({ haikus }) => {
               <div>
                 <div style={{ width: '45vw' }}>
                   <h2 className="fade-p3">Carson Bolt</h2>
-                  <p className="fade-p3 bio-p uk-dropcap uk-text-left">It's said the man eats rubber and drinks gasoline, just like the cars he loves so well.  A driver himself until an accident at the KFC Lunch buffet twenty years ago, he's since become a second father to many - and a first father to some.  Topped up on his signature "Goose Juice," the recipe for which is best left unsaid, he rules the race from his roost in the spotter tower - giving Mr. Racecar the edge he needs.</p>
+                  <p className="fade-p3 bio-p uk-text-left">It's said the man eats rubber and drinks gasoline, just like the cars he loves so well.  A driver himself until an accident at the KFC Lunch buffet twenty years ago, he's since become a second father to many - and a first father to some.  Topped up on his signature "Goose Juice," the recipe for which is best left unsaid, he rules the race from his roost in the spotter tower - giving Mr. Racecar the edge he needs.</p>
                 </div>
                 <div>
                   <blockquote cite="#" className="fade-p3">
@@ -86,7 +86,7 @@ const MainBody = ({ haikus }) => {
               <div>
                 <div style={{ width: '45vw' }}>
                   <h2 className="fade-p4">Kara</h2>
-                  <p className="fade-p4 bio-p uk-dropcap uk-text-left">Kara is many things. Wife to one, Mother to some and loved by all, she is the prettiest spotter in racing! Trust me, when you're riding one of these 3,200 pound steel beasts out of turn four at Daytona and you don't even know your own name anymore much less what the hell is going on outside your 200 MPH coffin, well, she's that calm voice in your ear like an angel of God calling you home the the finish line. </p>
+                  <p className="fade-p4 bio-p uk-text-left">Kara is many things. Wife to one, Mother to some and loved by all, she is the prettiest spotter in racing! Trust me, when you're riding one of these 3,200 pound steel beasts out of turn four at Daytona and you don't even know your own name anymore much less what the hell is going on outside your 200 MPH coffin, well, she's that calm voice in your ear like an angel of God calling you home the the finish line. </p>
                 </div>
                 <div>
                   <blockquote cite="#" className="fade-p4">
@@ -107,7 +107,7 @@ const MainBody = ({ haikus }) => {
               <div>
                 <div style={{ width: '45vw' }}>
                   <h2 className="fade-p5">Gabe "The Younger"</h2>
-                  <p className="fade-p5 bio-p uk-dropcap uk-text-left">Gabe The Younger leapt fully-formed from the trunk of a moving 1969 Pontiac GTO and he's been going fast ever since.  Catch the heir to the Gabir Motors family business in front of - and, crucially - behind the scenes, making websites like this one and spotting live on streams.</p>
+                  <p className="fade-p5 bio-p uk-text-left">Gabe The Younger leapt fully-formed from the trunk of a moving 1969 Pontiac GTO and he's been going fast ever since.  Catch the heir to the Gabir Motors family business in front of - and, crucially - behind the scenes, making websites like this one and spotting live on streams.</p>
                 </div>
                 <div>
                   <blockquote cite="#" className="fade-p5">
