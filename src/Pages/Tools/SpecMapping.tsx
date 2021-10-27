25c5fe8e043347088cefc87d753e6dfb6236249a
Gabe Krahulik <gabekrahulik@gmail.com>
2021-10-26 17:35:13 -0700

    updated spec mapping

--- src/Pages/Tools/SpecMapping.tsx
+++ src/Pages/Tools/SpecMapping.tsx
@@ -2,7 +2,6 @@
 import { useState, useEffect } from 'react'
 import Header from '../../components/Header'
 import Loading from "../../components/LoadingIcon/Loading";
-import { Standing } from '../../interfaces';
 
 const SpecMap = () => {
 	const [loading, setLoading] = useState(true);
@@ -17,6 +16,33 @@ const SpecMap = () => {
 	}, [])
 
 	useEffect(() => {
+		function drawCar(hexRGB: string, metalPct: number, roughPct: number) {
+			var rough = roughPct / 10;
+			var metal = metalPct / 10;
+	
+			if (hexRGB.substring(0, 1) === "#") {
+				hexRGB = hexRGB.substr(1);
+			}
+			var r = parseInt(hexRGB.substr(0, 2), 16) / 255;
+			var g = parseInt(hexRGB.substr(2, 2), 16) / 255;
+			var b = parseInt(hexRGB.substr(4, 2), 16) / 255;
+	
+			var black = document.getElementById("black");
+			var red = document.getElementById("red");
+			var green = document.getElementById("green");
+			var blue = document.getElementById("blue");
+	
+			var ctx = (document.getElementById("canvas") as HTMLCanvasElement)?.getContext('2d');
+			ctx?.clearRect(0, 0, (document.getElementById("canvas") as HTMLCanvasElement)?.width, (document.getElementById("canvas") as HTMLCanvasElement)?.height);
+	
+			if (ctx) {
+				drawImage(ctx, black, metal, rough);
+				drawImage(ctx, red, metal, rough, r, "lighten");
+				drawImage(ctx, green, metal, rough, g, "lighten");
+				drawImage(ctx, blue, metal, rough, b, "lighten");
+			}
+		}
+
 		drawCar(color, metallic, roughness)
 	}, [color, metallic, roughness])
 
@@ -33,35 +59,8 @@ const SpecMap = () => {
 		var xOffset = w * rough;
 		var yOffset = h * metal;
 
-		ctx.drawImage(image, xOffset, yOffset, w, h, 100, 100, w, h);
+		ctx.drawImage(image, xOffset, yOffset, w, h, 0, 0, 600, 400);
 		ctx.restore();
-	}
-
-	function drawCar(hexRGB: string, metalPct: number, roughPct: number) {
-		var rough = roughPct / 10;
-		var metal = metalPct / 10;
-
-		if (hexRGB.substring(0, 1) == "#") {
-			hexRGB = hexRGB.substr(1);
-		}
-		var r = parseInt(hexRGB.substr(0, 2), 16) / 255;
-		var g = parseInt(hexRGB.substr(2, 2), 16) / 255;
-		var b = parseInt(hexRGB.substr(4, 2), 16) / 255;
-
-		var black = document.getElementById("black");
-		var red = document.getElementById("red");
-		var green = document.getElementById("green");
-		var blue = document.getElementById("blue");
-
-		var ctx = (document.getElementById("canvas") as HTMLCanvasElement)?.getContext('2d');
-		ctx?.clearRect(0, 0, (document.getElementById("canvas") as HTMLCanvasElement)?.width, (document.getElementById("canvas") as HTMLCanvasElement)?.height);
-
-		if (ctx) {
-			drawImage(ctx, black, metal, rough);
-			drawImage(ctx, red, metal, rough, r, "lighten");
-			drawImage(ctx, green, metal, rough, g, "lighten");
-			drawImage(ctx, blue, metal, rough, b, "lighten");
-		}
 	}
 
 	const changeColor = (e: any) => setColor(e.target.value)
@@ -82,15 +81,17 @@ const SpecMap = () => {
 									<label htmlFor="color">Choose a Color:</label> <input type="color" id="color" value={color} onChange={changeColor} /><br />
 									<label htmlFor="metallic">Metallic</label> <input type="range" min="0" max="10" value={metallic / 10} id="metallic" onChange={changeMetal} /><br />
 									<label htmlFor="rough">Roughness</label> <input type="range" min="0" max="10" value={roughness / 10} id="rough" onChange={changeRough} /><br />
-									<img id="black" src="https://i.gabirmotors.com/siteimages/grid-black.jpg" style={{ display: "none" }} />
-									<img id="blue" src="https://i.gabirmotors.com/siteimages/grid-blue.jpg" style={{ display: "none" }} />
-									<img id="red" src="https://i.gabirmotors.com/siteimages/grid-red.jpg" style={{ display: "none" }} />
-									<img id="green" src="https://i.gabirmotors.com/siteimages/grid-green.jpg" style={{ display: "none" }} />
-									<canvas id="canvas" width="600" height="400"> </canvas>
+									<img id="black" src="https://i.gabirmotors.com/siteimages/grid-black.jpg" style={{ display: "none" }} alt = "grid-black" />
+									<img id="blue" src="https://i.gabirmotors.com/siteimages/grid-blue.jpg" style={{ display: "none" }} alt = "grid-blue" />
+									<img id="red" src="https://i.gabirmotors.com/siteimages/grid-red.jpg" style={{ display: "none" }} alt = "grid-red" />
+									<img id="green" src="https://i.gabirmotors.com/siteimages/grid-green.jpg" style={{ display: "none" }} alt = "grid-green" />
+									<canvas className = "tools-canvas" id="canvas" width="600" height="400"> </canvas>
 
-									<p id="color-value">Current Color: {color}</p>
-									<p id="metal-value">Metallic: {Math.ceil(metallic * 2.55).toString(16).toUpperCase()}</p>
-									<p id="rough-value">Roughness: {Math.ceil(roughness * 2.55).toString(16).toUpperCase()}</p>
+									<p className = "tools-number" id="color-value"><span className = "uk-text-bold">Current Color: </span>{color}</p>
+									<p className = "tools-number" id="metal-value"><span className = "uk-text-bold">Metallic: </span>{Math.ceil(metallic * 2.55).toString(16).toUpperCase()}</p>
+									<p className = "tools-number" id="rough-value"><span className = "uk-text-bold">Roughness: </span>{Math.ceil(roughness * 2.55).toString(16).toUpperCase()}</p>
+
+									<p className="uk-text-warning uk-text-bold">Warning: Some images may take a while to load</p>
 								</div>
 							</div>
 						</div>
